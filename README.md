ba6f3a4b0b5f6ae7c7d4ba28203c15226eb2f578
delivey <deliveydev@gmail.com>
2021-02-14 18:39:19 +0200

    misc improvements

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
-# Ratelimits
+## webhookq
+A queueing system for Discord's webhooks
 
-* 5 requests every 2-3 seconds
+## How to run
+1. If you don't have Node.js installed, install it from [here](https://nodejs.org/en/download/)
+2. Clone the repository by doing `git clone https://github.com/delivey/webhookq.git`
+3. Navigate to the repository's folder: `cd webhookq`
+4. Install the required dependencies: `npm i`
+5. Put your mongo connection URI, etc in `.env.example` and rename it to `.env`
+6. Run `npm start` to start the server.
+
+## How to use
+Assuming you're sending webhooks from localhost, and running this server on localhost, in your webhooks, change `https://discord.com` to `http://localhost:8080`. After doing this, all requests will go through the queue and you won't get ratelimited.
+
+## Discord's ratelimits
+* 5 requests every 2 seconds
+
+## Bugs / issues
+If you experience any issues or bugs, please open an issue.
+
--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,37 @@
+// Imports
 const send = require("./send.js")
+const mongoose = require("mongoose")
+const { Schema } = mongoose;
 log = console.log
+require('dotenv').config();
+
+// Connects to database
+const MONGO_URL = process.env.MONGO_URL
+mongoose.connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Creates the model and schema for the queue object
+const queueSchemaObj = {
+    identifier: String,
+    queuedCount: {
+        type: Number,
+        default: 0
+    },
+    ratelimitLeft: {
+        type: Number,
+        default: 0
+    }
+}
+
+const queueSchema = new Schema(queueSchemaObj)
+const Queue = mongoose.model('Queue', queueSchema);
+
+// Function for deleting everything from the database
+async function deleteAll() {
+    await Queue.deleteMany({}).exec();
+}
+
+const rlRequests = 5 // Amount of requests that can be send before ratelimit (dependent on rlSeconds)
+const rlSeconds = 2 // In how many seconds said requests have to be sent
 
 module.exports = function(app){
 
@@ -11,20 +43,41 @@ module.exports = function(app){
 
     app.post("/api/webhooks/:server/:webhook", async function (req, res) {
 
-        const webhookUrl = `https://discord.com/api/webhooks/${req.params.server}/${req.params.webhook}`
+        const identifier = `${req.params.server}/${req.params.webhook}`
+        const webhookUrl = `https://discord.com/api/webhooks/${identifier}`
         const hookBody = req.body
 
-        // Sends webhook to discord
-        const response = await send.sendWebhook(webhookUrl, hookBody)
+        // Status of the current webhook (queued or sent)
+        var status;
+
+        // Checks if webhook exists in the database
+        var hookExists = true;
+        const idfres = await Queue.findOne({ identifier: identifier }).exec();
+        if (idfres === null) hookExists = false;
+
+        // If webhook isn't in the database
+        if (!hookExists) {
+            await Queue.create({ identifier: identifier })
+            status = "sent"
+        } else {
+            status = "sent"
+        }
+
+        // Sends webhook to discord if allowed
+        if (status === "sent") {
+            const response = await send.sendWebhook(webhookUrl, hookBody)
+            const reqsLeft = response.headers["x-ratelimit-remaining"]
+            const resetAfter = response.headers["x-ratelimit-reset-after"]
+        }
 
         // Constructs the response
         var respObj;
-        if (response.status === 204) {
+        if (status === "sent") {
             respObj = {
                 "status": "sent",
                 "milisecondsLeft": 0
             }
-        } else if (response.status === 429) {
+        } else {
             respObj = {
                 "status": "queued",
                 "milisecondsLeft": 999
