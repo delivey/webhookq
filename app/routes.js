d7becd8783bce32e443eceb5250e27165de89bab
delivey <deliveydev@gmail.com>
2021-02-15 12:00:40 +0200

    timing seems more or less done

--- app/routes.js
+++ app/routes.js
@@ -1,16 +1,19 @@
 // Imports
-const send = require("./send.js")
-const mongoose = require("mongoose")
+const send = require("./send.js");
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
-log = console.log
-require('dotenv').config();
+log = console.log;
+require("dotenv").config();
 
 // Connects to database
-const MONGO_URL = process.env.MONGO_URL
-mongoose.connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+const MONGO_URL = process.env.MONGO_URL;
+mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 // Connects to agenda
-const Agenda = require("agenda")
+const Agenda = require("agenda");
 const agenda = new Agenda({ db: { address: MONGO_URL } });
 
 // Creates the model and schema for the queue object
@@ -18,105 +21,124 @@ const queueSchemaObj = {
     identifier: String,
     queuedCount: {
         type: Number,
-        default: 0
+        default: 0,
     },
     ratelimitLeft: {
         type: Number,
-        default: 5
+        default: 5,
     },
     // Unix time of when queue was last updated
     lastUpdated: {
         type: Number,
-        default: 0
-    }
-}
+        default: 0,
+    },
+};
 
-const queueSchema = new Schema(queueSchemaObj)
-const Queue = mongoose.model('Queue', queueSchema);
+const queueSchema = new Schema(queueSchemaObj);
+const Queue = mongoose.model("Queue", queueSchema);
 
 // Function for deleting everything from the database
 async function deleteAll() {
     await Queue.deleteMany({}).exec();
 }
 
-deleteAll()
+deleteAll();
 
 const getUnix = () => {
-    return Math.round((new Date()).getTime() / 1000);
-}
+    return Math.round(new Date().getTime() / 1000);
+};
 
-const rlRequests = 5 // Amount of requests that can be send before ratelimit (dependent on rlSeconds)
-const rlSeconds = 2 // In how many seconds said requests have to be sent
+const rlRequests = 5; // Amount of requests that can be send before ratelimit (dependent on rlSeconds)
+const rlSeconds = 2; // In how many seconds said requests have to be sent
 
-module.exports = function(app){
-
-    app.get("/", function(req, res){
-        log("Got request to homepage")
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({ "message": "Hello World!" }));
+module.exports = function (app) {
+    app.get("/", function (req, res) {
+        log("Got request to homepage");
+        res.setHeader("Content-Type", "application/json");
+        res.end(JSON.stringify({ message: "Hello World!" }));
     });
 
     app.post("/api/webhooks/:server/:webhook", async function (req, res) {
-
-        const identifier = `${req.params.server}/${req.params.webhook}`
-        const webhookUrl = `https://discord.com/api/webhooks/${identifier}`
-        const hookBody = req.body
+        const identifier = `${req.params.server}/${req.params.webhook}`;
+        const webhookUrl = `https://discord.com/api/webhooks/${identifier}`;
+        const hookBody = req.body;
 
         // Status of the current webhook (queued or sent)
         // sent - will be sent instantly
         // queued - will be sent after passes queue
         var status;
         // Miliseconds left until webhook will be sent
-        var milisecondsLeft = 42069;
+        var milisecondsLeft;
 
         // Checks if webhook exists in the database
         var hookExists = true;
         const idfres = await Queue.findOne({ identifier: identifier }).exec();
         if (idfres === null) hookExists = false;
-        else log(idfres)
+        else log(idfres);
 
         // If webhook isn't in the database
         if (!hookExists) {
-            await Queue.create({ identifier: identifier })
-            status = "sent"
+            await Queue.create({ identifier: identifier });
+            status = "sent";
         } else {
-            const date = getUnix()
+            const date = getUnix();
             // Checks if last webhook was sent more than 2 seconds ago
             if (date - idfres.lastUpdated >= rlSeconds) {
                 // Resets left requests and updates date
-                await Queue.updateOne({ identifier: identifier }, { ratelimitLeft: 5, lastUpdated: date });
-                status = "sent"
+                await Queue.updateOne(
+                    { identifier: identifier },
+                    { ratelimitLeft: 5, lastUpdated: date }
+                );
+                status = "sent";
             } else {
                 if (idfres.ratelimitLeft > 0) status = "sent";
-                else status = "queued"
+                else status = "queued";
             }
         }
 
         async function fullSend() {
-            const response = await send.sendWebhook(webhookUrl, hookBody)
-            const reqsLeft = Number(response.headers["x-ratelimit-remaining"])
+            const response = await send.sendWebhook(webhookUrl, hookBody);
+            const reqsLeft = Number(response.headers["x-ratelimit-remaining"]);
             // const resetAfter = response.headers["x-ratelimit-reset-after"]
-            const date = getUnix()
-            await Queue.updateOne({ identifier: identifier }, { ratelimitLeft: reqsLeft, lastUpdated: date });
+            const date = getUnix();
+            await Queue.updateOne(
+                { identifier: identifier },
+                { ratelimitLeft: reqsLeft, lastUpdated: date }
+            );
         }
 
         agenda.define("fullSend", async (job) => {
-            await fullSend()
+            await fullSend();
         });
 
         // Sends webhook to discord if allowed
         if (status === "sent") {
-           await fullSend()
-        } else { // If request got queue'd
-            await Queue.updateOne({ identifier: identifier }, {$inc: { queuedCount: 1} });
+            milisecondsLeft = 0;
+            await fullSend();
+        } else {
+            // If request got queue'd
+            await Queue.updateOne({ identifier: identifier }, { $inc: { queuedCount: 1 } });
+
+            // Could maybe be removed, just trying to get as minimal latency as possible
+            const idfresn = await Queue.findOne({
+                identifier: identifier,
+            }).exec();
+
+            const queuedCount = idfresn.queuedCount;
+
+            // Calculates how much time is left until webhook should be sent
+            let seconds = Math.round(queuedCount / rlRequests) * rlSeconds;
+            milisecondsLeft = seconds * 1000;
+
+            await agenda.start();
         }
 
         // Constructs the response
         var respObj = {
-            "status": status,
-            "milisecondsLeft": milisecondsLeft
+            status: status,
+            milisecondsLeft: milisecondsLeft,
         };
 
         res.end(JSON.stringify(respObj));
-    })
-}
+    });
+};
