eaf411c42f51800e5084b262336411965234f89d
delivey <deliveydev@gmail.com>
2021-03-07 14:42:16 +0200

    better benchmarking

--- benchmarks/queued.py
+++ benchmarks/queued.py
@@ -1,20 +1,41 @@
 # Benchmarks queued discord requests
 # Server must be running for it to work
 import requests
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
-webhook_url = ""
+class Benchmark:
 
-# Amount of requests that should be sent
-requests_total = 100
+    def get_webhook_url(self):
 
-def send_webhook(number=0):
-    data = {
-        "content": f"TEST {number}"
-    }
-    r = requests.post(webhook_url, json=data)
-    return r
+        original_url = os.getenv("TEST_WEBHOOK_URL")
+        server_url = os.getenv("SERVER_URL")
+        webhook_url = original_url.replace("https://discordapp.com", server_url)
 
-for i in range(1, requests_total+1):
-    r = send_webhook(i)
-    data = r.json()
-    print(f"Sent request {i}. Status: {data['status']}. Seconds left: {data['secondsLeft']}")
+        return webhook_url
+
+    def __init__(self):
+        self.requests_total = 0
+        self.requests_sent = 0
+        self.webhook_url = self.get_webhook_url()
+
+    # Sends a webhook with benchmark content
+    def send_webhook(self, number=0):
+        data = {
+            "content": f"TEST {number}"
+        }
+        r = requests.post(self.webhook_url, json=data)
+        self.requests_sent += 1
+        return r
+
+    # Sends x webhooks
+    def run_one(self, x):
+        for i in range(1, x+1):
+            self.requests_total += 1
+            r = self.send_webhook(i)
+            data = r.json()
+            print(f"Sent request {i}. Status: {data['status']}. Seconds left: {data['secondsLeft']}")
+
+benchmark = Benchmark()
+benchmark.run_one(1000)
